153d0b2b147bef4395952d591011e376d4c7675c
wojiaomeli <147832537+wojiaomeli@users.noreply.github.com>
2024-07-01 15:13:14 +0200

    Delete node_modules directory

--- node_modules/@storybook/nextjs/dist/chunk-NQ32RFK2.mjs
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import { ImageContext as ImageContext$1 } from '@storybook/nextjs/dist/image-context';
-
-var ImageContext=ImageContext$1,ImageDecorator=(Story,{parameters})=>parameters.nextjs?.image?React.createElement(ImageContext.Provider,{value:parameters.nextjs.image},React.createElement(Story,null)):React.createElement(Story,null);
-
-export { ImageDecorator };
